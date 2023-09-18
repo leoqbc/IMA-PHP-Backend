95782f05f2c325e139c44a3413566128324f60d8
Leonardo Tumadjian <tumadjian@gmail.com>
2023-09-18 19:00:09 -0300

    Adiciona desafio da aula5

---
+++ aula5/desafio/index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    Crie um formulário de cadastramento de cliente para exibir na tela seguinte os dados:
+
+    1. Nome
+    2. Email
+    3. Telefone
+    4. Genero
+
+    Criar um arquivo chamado exibe_cliente.php ao confirmar o cadastro você deve redirecionar a pessoa para a página
+    e exibir seu dados.
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de cadastro</title>
+</head>
+<body>
+    
+</body>
+</html>
